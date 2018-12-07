0a4f433e9a56dd82f51eb1a137d4eee40c9f6576
AyreLabs <45647698+AyreLabs@users.noreply.github.com>
2018-12-07 12:18:43 +1100

    Update NautilusKeyboardProtocol.java

--- NautilusKeyboardClient/NautilusKeyboardProtocol.java
+++ NautilusKeyboardClient/NautilusKeyboardProtocol.java
@@ -1,8 +1,22 @@
+/*----------------------------------------------------------------------------------------
+    PROJECT
+    -------
+    Project Nautilus
+    DESCRIPTION
+    -----------
+    ...
+    AUTHOR
+    ------
+    Ayre Labs (2018)
+----------------------------------------------------------------------------------------*/
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class NautilusKeyboardProtocol {
 
+  /*----------------------------------------------------------------------------------------
+  Static Public Methods
+  ----------------------------------------------------------------------------------------*/
   public static String keyPressEventMessageToStringWithKeyEvent(KeyEvent keyEventInQuestion) {
       return String.format("%d", keyEventInQuestion.getKeyCode());
   }
