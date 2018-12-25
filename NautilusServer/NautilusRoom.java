6100c0d89d7a4ef495e4100a1861b7e88ff5c73c
Ayre Labs <AyreLabs@outlook.com>
2018-12-25 13:28:19 +1100

    .

--- NautilusServer/NautilusRoom.java
+++ NautilusServer/NautilusRoom.java
@@ -1,75 +1,101 @@
+//----------------------------------------------------------------------------------------
+//    PROJECT
+//    -------
+//    Project Nautilus
+//
+//    AUTHOR
+//    ------
+//    Ayre Labs (2018)
+//----------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------
+// IMPORTS
+//----------------------------------------------------------------------------------------
 import java.util.ArrayList;
 
+//----------------------------------------------------------------------------------------
+// CLASS DEFINITION
+//----------------------------------------------------------------------------------------
 public class NautilusRoom implements NautilusRoomObserver, NautilusKeyReceiver {
-  private ArrayList<NautilusRoomTerminal> terminalsInTheNautilusRoom = new ArrayList<NautilusRoomTerminal>();
-  private NautilusRoomUpdateListener nautilusRoomUpdateListener = null;
+  
+    private ArrayList<NautilusRoomTerminal> terminalsInTheNautilusRoom;
+    private NautilusRoomUpdateListener nautilusRoomUpdateListener;
 
-  private NautilusRoom() {
-    this.startRunningThreadToContinuouslyUpdateDirtyTerminalDisplays();
-  }
+    private NautilusRoom() {
+        this.terminalsInTheNautilusRoom = new ArrayList<NautilusRoomTerminal>();
+        this.nautilusRoomUpdateListener = null;
+        this.startRunningThreadToContinuouslyUpdateDirtyTerminalDisplays();
+    }
 
-  private void startRunningThreadToContinuouslyUpdateDirtyTerminalDisplays() {
-    new Thread() {
-        public void run() {
-          while(true) {
-            try {
-              Thread.sleep(200);
-            } catch (Exception exception) {
-              exception.printStackTrace();
+    private void startRunningThreadToContinuouslyUpdateDirtyTerminalDisplays() {
+        Thread threadForContinuouslyUpdateDirtyTerminalDisplays = this.createThreadForContinuouslyUpdateDirtyTerminalDisplays();
+        threadForContinuouslyUpdateDirtyTerminalDisplays.start();
+    }
+
+    private Thread createThreadForContinuouslyUpdateDirtyTerminalDisplays() {
+        return new Thread() {
+            public void run() {
+                NautilusRoom.this.continuouslyUpdateDirtyTerminalDisplays();
             }
-            NautilusRoom.this.updateDirtyTerminalDisplays();
-          }
+        };
+    }
+
+    public void continuouslyUpdateDirtyTerminalDisplays() {
+        while(true) {
+            this.attemptSleepForMilliSeconds(200);
+            this.updateDirtyTerminalDisplays();
         }
-      }.start();
-  }
-
-  public static NautilusRoom newEmptyNautilusRoom() {
-    return new NautilusRoom();
-  }
-
-  public void addTerminalToRoom(NautilusRoomTerminal terminalToAddToRoom) {
-    this.terminalsInTheNautilusRoom.add(terminalToAddToRoom);
-    this.notifyNautilusRoomUpdateListenersThatATerminalWasUpdated(terminalToAddToRoom);
-  }
-
-  public void receiveNautilusKeyPressForNautilusRoomNumber(NautilusKey keyThatWasPressed, int nautilusRoomNumber) {
-    boolean terminalExistsCorrespondingToRoomNumber = this.terminalsInTheNautilusRoom.size() > nautilusRoomNumber && nautilusRoomNumber >= 0;
-    if (terminalExistsCorrespondingToRoomNumber) {
-      NautilusRoomTerminal theTerminalOnWhichToPressAKey = this.terminalsInTheNautilusRoom.get(nautilusRoomNumber);
-      theTerminalOnWhichToPressAKey.pressKeyOnTerminal(keyThatWasPressed);
     }
-  }
 
-  public ArrayList<NautilusRoomTerminal>  getCurrentNautilusRoomTerminals() {
-    return this.terminalsInTheNautilusRoom;
-  }
-
-  public void setNautilusRoomUpdateListener(NautilusRoomUpdateListener nautilusRoomUpdateListener) {
-    this.nautilusRoomUpdateListener = nautilusRoomUpdateListener;
-  }
-
-  private void notifyNautilusRoomUpdateListenersThatATerminalWasUpdated(NautilusRoomTerminal nautilusRoomTerminalThatWasUpdated) {
-    if (this.nautilusRoomUpdateListener != null) {
-      this.nautilusRoomUpdateListener.receivedNotificationThatANautilusRoomTerminalWasUpdated(nautilusRoomTerminalThatWasUpdated);
+    private void attemptSleepForMilliSeconds(int sleepTimeInMilliSeconds) {
+        try {
+            Thread.sleep(sleepTimeInMilliSeconds);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
     }
-  }  
 
-
-  private void updateDirtyTerminalDisplays() {
-    for (NautilusRoomTerminal terminalInTheNautilusRoom : this.terminalsInTheNautilusRoom) {
-      if (terminalInTheNautilusRoom.terminalHasDirtyDisplay()) {
-        terminalInTheNautilusRoom.updateDirtyTerminalDisplay();
-        this.notifyNautilusRoomUpdateListenersThatATerminalWasUpdated(terminalInTheNautilusRoom);
-      }
+    public static NautilusRoom newEmptyNautilusRoom() {
+        return new NautilusRoom();
     }
-  }
 
+    public void addTerminalToRoom(NautilusRoomTerminal terminalToAddToRoom) {
+        this.terminalsInTheNautilusRoom.add(terminalToAddToRoom);
+        this.notifyNautilusRoomUpdateListenersThatATerminalWasUpdated(terminalToAddToRoom);
+    }
+
+    public void receiveNautilusKeyPressForNautilusRoomNumber(NautilusKey keyThatWasPressed, int nautilusRoomNumber) {
+        boolean terminalExistsCorrespondingToRoomNumber = this.terminalsInTheNautilusRoom.size() > nautilusRoomNumber && nautilusRoomNumber >= 0;
+        if (terminalExistsCorrespondingToRoomNumber) {
+            NautilusRoomTerminal theTerminalOnWhichToPressAKey = this.terminalsInTheNautilusRoom.get(nautilusRoomNumber);
+            theTerminalOnWhichToPressAKey.pressKeyOnTerminal(keyThatWasPressed);
+        }
+    }  
+
+    public ArrayList<NautilusRoomTerminal>  getCurrentNautilusRoomTerminals() {
+        return this.terminalsInTheNautilusRoom;
+    }
+
+    public void setNautilusRoomUpdateListener(NautilusRoomUpdateListener nautilusRoomUpdateListener) {
+        this.nautilusRoomUpdateListener = nautilusRoomUpdateListener;
+    }
+
+    private void notifyNautilusRoomUpdateListenersThatATerminalWasUpdated(NautilusRoomTerminal nautilusRoomTerminalThatWasUpdated) {
+        if(this.nautilusRoomUpdateListener != null) {
+            this.nautilusRoomUpdateListener.receivedNotificationThatANautilusRoomTerminalWasUpdated(nautilusRoomTerminalThatWasUpdated);
+        }
+    }  
+
+    private void updateDirtyTerminalDisplays() {
+        for (NautilusRoomTerminal terminalInTheNautilusRoom : this.terminalsInTheNautilusRoom) {
+            this.updateDirtyTerminalDisplayForDisplay(terminalInTheNautilusRoom);
+        } 
+    }
+
+    private void updateDirtyTerminalDisplayForDisplay(NautilusRoomTerminal terminalInTheNautilusRoom) {
+        if (terminalInTheNautilusRoom.terminalHasDirtyDisplay()) {
+            terminalInTheNautilusRoom.updateDirtyTerminalDisplay();
+            this.notifyNautilusRoomUpdateListenersThatATerminalWasUpdated(terminalInTheNautilusRoom);
+        }
+    }
 }
-
-
-
-
-
-
-
-
