b9b5a3247883c00a4b35f6dc8e39d2795a19ba4a
Ayre Labs <AyreLabs@outlook.com>
2018-12-25 15:57:42 +1100

    .

--- NautilusServer/NautilusRoomTerminal.java
+++ NautilusServer/NautilusRoomTerminal.java
@@ -1,3 +1,16 @@
+//----------------------------------------------------------------------------------------
+//    PROJECT
+//    -------
+//    Project Nautilus
+//
+//    AUTHOR
+//    ------
+//    Ayre Labs (2018)
+//----------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------
+// IMPORTS
+//----------------------------------------------------------------------------------------
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -8,128 +21,128 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
+//----------------------------------------------------------------------------------------
+// CLASS DEFINITION
+//----------------------------------------------------------------------------------------
 public class NautilusRoomTerminal {
 
-  private int terminalID = 0;
+    private int terminalID;
+    private String currentTerminalWindowDisplayString;
+    private NautilusRoomTerminalDimensions roomDimensions;
+    private boolean terminalDisplayNeedsToBeUpdated;
+    private long systemTimeThatTerminalDisplayWasLastUpdated;
+    private NautilusTerminalService terminalServiceForThisNautilusTerminal;
 
-  private String currentTerminalWindowDisplayString = "";
+    private final static long MS_DELAY_BETWEEN_REGULAR_TERMINAL_DISPLAY_UPDATES = 3000;
 
-  private double positionX = 0;
-  private double positionY = 0;
-  private double positionZ = 0;
-  private double rotationX = 0;
-  private double rotationY = 0;
-  private double rotationZ = 0;
-  private double spatialTerminalWidth = 0;
-
-  private boolean terminalDisplayNeedsToBeUpdated = true;
-  private long systemTimeThatTerminalDisplayWasLastUpdated = 0;
-  private final static long MS_DELAY_BETWEEN_REGULAR_TERMINAL_DISPLAY_UPDATES = 3000;
-
-  private NautilusTerminalService terminalServiceForThisNautilusTerminal;
-
-
-  public static String getNautilusFormatDescriptionForTerminalConfigurationString() {
-    return "terminal_initilisation_string~position-x~position-y~position-z~rotation-x~rotation-y~rotation-z~spatialTerminalWidth";
-  }
-
-  private NautilusRoomTerminal(String terminalConfigurationString, int terminalID) {
-    this.terminalID = terminalID;
-    this.terminalServiceForThisNautilusTerminal = NautilusTerminalService.startTerminalServiceForTerminalWithID(terminalID);
-
-    String[] terminalConfigurationStringComponents = terminalConfigurationString.split("~");
-    boolean terminalConfigurationStringIsValid = terminalConfigurationStringComponents.length >= 7;
-    if (terminalConfigurationStringIsValid) {
-      try {
-        this.positionX = Double.valueOf(terminalConfigurationStringComponents[1]);
-        this.positionY = Double.valueOf(terminalConfigurationStringComponents[2]);
-        this.positionZ = Double.valueOf(terminalConfigurationStringComponents[3]);
-        this.rotationX = Double.valueOf(terminalConfigurationStringComponents[4]);
-        this.rotationY = Double.valueOf(terminalConfigurationStringComponents[5]);
-        this.rotationZ = Double.valueOf(terminalConfigurationStringComponents[6]);
-        this.spatialTerminalWidth = Double.valueOf(terminalConfigurationStringComponents[7]);
-      } catch (Exception exception) {
-        exception.printStackTrace();
-      }
+    public static String getNautilusFormatDescriptionForTerminalConfigurationString() {
+        return "terminal_initilisation_string~position-x~position-y~position-z~rotation-x~rotation-y~rotation-z~spatialTerminalWidth";
     }
-  }
 
-  public static NautilusRoomTerminal newNautilusTerminalFromConfigurationStringAndTerminalID(String terminalConfigurationString, int terminalID) {
-    return new NautilusRoomTerminal(terminalConfigurationString, terminalID);
-  }
-
-  public void pressKeyOnTerminal(NautilusKey keyThatWasPressed) {
-    if (keyThatWasPressed.isBackspaceKey()) {
-        this.terminalServiceForThisNautilusTerminal.runResponselessNautilusTerminalServiceCommand("InjectBackspaceIntoSTDINForTerminalWithID");
-    } else if (keyThatWasPressed.isEnterKey()) {
-        this.terminalServiceForThisNautilusTerminal.runResponselessNautilusTerminalServiceCommand("InjectReturnIntoSTDINForTerminalWithID");
-    } else if (keyThatWasPressed.isEscapeKey()) {
-        this.terminalServiceForThisNautilusTerminal.runResponselessNautilusTerminalServiceCommand("InjectEscapeIntoSTDINForTerminalWithID");
-    } else {
-        this.terminalServiceForThisNautilusTerminal.runResponselessNautilusTerminalServiceCommandWithInputParameter("InjectSTDINForTerminalWithIDAndInjectedInput", "\"" + keyThatWasPressed.getStringRepresentationOfKey() + "\"");
+    private NautilusRoomTerminal(String terminalConfigurationString, int terminalID) {
+        this.setupRoomStaticFeatures(terminalConfigurationString, terminalID);
+        this.setupRoomDynamicFeatures(terminalID);
     }
-    this.terminalDisplayNeedsToBeUpdated = true;
-  }
 
-  private String stringByRemovingLastCharacter(String str) {
-    if (str != null && str.length() > 0) {
-        str = str.substring(0, str.length() - 1);
+    private void setupRoomStaticFeatures(String terminalConfigurationString, int terminalID) {
+        this.currentTerminalWindowDisplayString = "";
+        this.terminalID = terminalID;
+        this.roomDimensions = NautilusRoomTerminalDimensions();
+        this.setupRoomDimensionsFromTerminalConfigurationString(terminalConfigurationString);        
     }
-    return str;
-  }
 
-
-  public String constructNautilusRoomTerminalPositionStateUpdateMessage() {
-    return NautilusVRProtocol.nautilusRoomTerminalPositionStateUpdateMessageWithTerminalIDPositionXYZAndRotationXYZAndSpatialTerminalWidth(this.terminalID, this.positionX, this.positionY, this.positionZ, this.rotationX, this.rotationY, this.rotationZ, this.spatialTerminalWidth);
-  } 
-
-  public String constructNautilusRoomTerminalDisplayStateUpdateMessage() {
-    String currentTerminalDisplay = this.constructCurrentDisplayString();
-    //System.out.printf("AAAAAAAAAAA:%s\n", currentTerminalDisplay);
-    return NautilusVRProtocol.nautilusRoomTerminalDisplayStateUpdateMessageWithTerminalIDAndDisplayString(this.terminalID, currentTerminalDisplay);
-  }
-
-  private String constructCurrentDisplayString() {
-      return this.currentTerminalWindowDisplayString;
-  }
-
-  public boolean terminalHasDirtyDisplay() {
-    //return false;
-    return this.terminalDisplayNeedsToBeUpdated 
-      || (System.currentTimeMillis() - this.systemTimeThatTerminalDisplayWasLastUpdated) > MS_DELAY_BETWEEN_REGULAR_TERMINAL_DISPLAY_UPDATES;
-  }
-
-  public void updateDirtyTerminalDisplay() {
-    this.updateRoomTerminalWithNewTerminalDisplayInformation();
-    this.terminalDisplayNeedsToBeUpdated = false;
-    this.systemTimeThatTerminalDisplayWasLastUpdated = System.currentTimeMillis();
-  }
-
-
-  private void updateRoomTerminalWithNewTerminalDisplayInformation() {
-    String terminalDisplayInfoDump = this.terminalServiceForThisNautilusTerminal.runNautilusTerminalServiceCommandAndReturningResult("PullDisplayForTerminalWithID");
-    this.currentTerminalWindowDisplayString = terminalDisplayInfoDump;
-    //System.out.printf("AAA: %s\n", terminalDisplayInfoDump);
-
-
-/*
-    String[] terminalDisplayInfoDumpComponents = terminalDisplayInfoDump.split("\n", -1);
-    //System.out.printf("AAA: %s\n", terminalDisplayInfoDumpComponents[0]);
-    String viewportHeightString = (terminalDisplayInfoDumpComponents[0].split(","))[2];
-    int viewportHeight = Integer.parseInt(viewportHeightString);
-    //String terminalStringBuffer = terminalDisplayInfoDumpComponents[1];
-    String[] terminalStringBufferLines = terminalDisplayInfoDumpComponents;//terminalStringBuffer.split("\n");
-    String terminalDisplayString = "";
-    for (int terminalStringBufferViewportLineIndex = 0; terminalStringBufferViewportLineIndex< viewportHeight; terminalStringBufferViewportLineIndex++) {
-  //System.out.printf("QQQ:linesLength:%d,height:%d,index:%d\n", terminalStringBufferLines.length, viewportHeight, terminalStringBufferViewportLineIndex);
-        terminalDisplayString += terminalStringBufferLines[terminalStringBufferViewportLineIndex+(terminalStringBufferLines.length-viewportHeight-1)] + "\n";
+    private void setupRoomDynamicFeatures(int terminalID) {
+        this.terminalServiceForThisNautilusTerminal = NautilusTerminalService.startTerminalServiceForTerminalWithID(terminalID);
+        this.terminalDisplayNeedsToBeUpdated = true;
+        this.systemTimeThatTerminalDisplayWasLastUpdated = 0;
     }
-    this.currentTerminalWindowDisplayString = terminalDisplayString;
-  */
 
-  }
+    private void setupRoomDimensionsFromTerminalConfigurationString() {
+        String[] terminalConfigurationStringComponents = terminalConfigurationString.split("~");
+        boolean terminalConfigurationStringIsValid = terminalConfigurationStringComponents.length >= 7;
+        if (terminalConfigurationStringIsValid) {
+            this.attemptToSetRoomDimensionsValuesFromTerminalConfigurationStringComponents(terminalConfigurationStringComponents);
+        }
+    }
 
+    private void attemptToSetRoomDimensionsValuesFromTerminalConfigurationStringComponents(String[] terminalConfigurationStringComponents) {
+        try {
+            this.setRoomDimensionsValuesFromTerminalConfigurationStringComponents(terminalConfigurationStringComponents);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    private void setRoomDimensionsValuesFromTerminalConfigurationStringComponents(String[] terminalConfigurationStringComponents) {
+        this.setRoomPositionFromTerminalConfigurationStringComponents(terminalConfigurationStringComponents);
+        this.setRoomRotationFromTerminalConfigurationStringComponents(terminalConfigurationStringComponents);
+    }
+
+    private void setRoomPositionFromTerminalConfigurationStringComponents(String[] terminalConfigurationStringComponents) {
+        this.roomDimensions.positionX = Double.valueOf(terminalConfigurationStringComponents[1]);
+        this.roomDimensions.positionY = Double.valueOf(terminalConfigurationStringComponents[2]);
+        this.roomDimensions.positionZ = Double.valueOf(terminalConfigurationStringComponents[3]);
+        this.roomDimensions.spatialTerminalWidth = Double.valueOf(terminalConfigurationStringComponents[7]);
+    }
+
+    private void setRoomRotationFromTerminalConfigurationStringComponents(String[] terminalConfigurationStringComponents) {
+        this.roomDimensions.rotationX = Double.valueOf(terminalConfigurationStringComponents[4]);
+        this.roomDimensions.rotationY = Double.valueOf(terminalConfigurationStringComponents[5]);
+        this.roomDimensions.rotationZ = Double.valueOf(terminalConfigurationStringComponents[6]); 
+    }
+
+    public static NautilusRoomTerminal newNautilusTerminalFromConfigurationStringAndTerminalID(String terminalConfigurationString, int terminalID) {
+        return new NautilusRoomTerminal(terminalConfigurationString, terminalID);
+    }
+
+    public void pressKeyOnTerminal(NautilusKey keyThatWasPressed) {
+        if (keyThatWasPressed.isBackspaceKey()) {
+            this.terminalServiceForThisNautilusTerminal.runResponselessNautilusTerminalServiceCommand("InjectBackspaceIntoSTDINForTerminalWithID");
+        } else if (keyThatWasPressed.isEnterKey()) {
+            this.terminalServiceForThisNautilusTerminal.runResponselessNautilusTerminalServiceCommand("InjectReturnIntoSTDINForTerminalWithID");
+        } else if (keyThatWasPressed.isEscapeKey()) {
+            this.terminalServiceForThisNautilusTerminal.runResponselessNautilusTerminalServiceCommand("InjectEscapeIntoSTDINForTerminalWithID");
+        } else {
+            this.terminalServiceForThisNautilusTerminal.runResponselessNautilusTerminalServiceCommandWithInputParameter("InjectSTDINForTerminalWithIDAndInjectedInput", "\"" + keyThatWasPressed.getStringRepresentationOfKey() + "\"");
+        } 
+        this.terminalDisplayNeedsToBeUpdated = true;
+    }
+
+    private String stringByRemovingLastCharacter(String stringInQuestion) {
+        if (stringInQuestion != null && stringInQuestion.length() > 0) {
+            stringInQuestion = stringInQuestion.substring(0, stringInQuestion.length() - 1);
+        }
+        return stringInQuestion;
+    }
+
+    public String constructNautilusRoomTerminalPositionStateUpdateMessage() {
+        return NautilusVRProtocol.nautilusRoomTerminalPositionStateUpdateMessageWithTerminalIDPositionXYZAndRotationXYZAndSpatialTerminalWidth(this.terminalID, this.roomDimensions.positionX, this.roomDimensions.positionY, this.roomDimensions.positionZ, this.roomDimensions.rotationX, this.roomDimensions.rotationY, this.roomDimensions.rotationZ, this.roomDimensions.spatialTerminalWidth);
+    } 
+
+    public String constructNautilusRoomTerminalDisplayStateUpdateMessage() {
+        String currentTerminalDisplay = this.constructCurrentDisplayString();
+        System.out.printf("AAAAAAAAAAA:%s\n", currentTerminalDisplay);
+        return NautilusVRProtocol.nautilusRoomTerminalDisplayStateUpdateMessageWithTerminalIDAndDisplayString(this.terminalID, currentTerminalDisplay);
+    }
+
+    private String constructCurrentDisplayString() {
+        return this.currentTerminalWindowDisplayString;
+    }
+
+    public boolean terminalHasDirtyDisplay() {
+        //return false;
+        return this.terminalDisplayNeedsToBeUpdated 
+            || (System.currentTimeMillis() - this.systemTimeThatTerminalDisplayWasLastUpdated) > MS_DELAY_BETWEEN_REGULAR_TERMINAL_DISPLAY_UPDATES;
+    }
+
+    public void updateDirtyTerminalDisplay() {
+        this.updateRoomTerminalWithNewTerminalDisplayInformation();
+        this.terminalDisplayNeedsToBeUpdated = false;
+        this.systemTimeThatTerminalDisplayWasLastUpdated = System.currentTimeMillis();
+    }
+
+    private void updateRoomTerminalWithNewTerminalDisplayInformation() {
+         String terminalDisplayInfoDump = this.terminalServiceForThisNautilusTerminal.runNautilusTerminalServiceCommandAndReturningResult("PullDisplayForTerminalWithID");
+        this.currentTerminalWindowDisplayString = terminalDisplayInfoDump;
+    }
 }
-
-
-
---
+++ NautilusServer/NautilusRoomTerminalDimensions.java
@@ -0,0 +1,31 @@
+//----------------------------------------------------------------------------------------
+//    PROJECT
+//    -------
+//    Project Nautilus
+//
+//    AUTHOR
+//    ------
+//    Ayre Labs (2018)
+//----------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------
+// IMPORTS
+//----------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------
+// CLASS DEFINITION
+//----------------------------------------------------------------------------------------
+public class NautilusRoomTerminalDimensions {
+
+	public double positionX = 0;
+    public double positionY = 0;
+    public double positionZ = 0;
+    public double rotationX = 0;
+    public double rotationY = 0;
+    public double rotationZ = 0;
+    public double spatialTerminalWidth = 0;
+
+    public NautilusRoomTerminalDimensions() {
+    	/* Nothing */
+    }
+}
